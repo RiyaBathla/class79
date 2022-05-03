dcaf09494e3fdb6cdfec9e294f927ee700ddd3fd
RiyaBathla <92024130+RiyaBathla@users.noreply.github.com>
2022-05-03 15:45:46 +0530

    Add files via upload

---
+++ class 79js.js
@@ -0,0 +1,26 @@
+name_of_the_student_array = [];
+
+function submit()
+
+{
+var name_1 = document.getElementById("name_of_the_student1").value;
+var name_2 = document.getElementById("name_of_the_student2").value;
+var name_3 = document.getElementById("name_of_the_student3").value;
+var name_4 = document.getElementById("name_of_the_student4").value;
+
+name_of_the_student_array.push(name_1);
+name_of_the_student_array.push(name_2);
+name_of_the_student_array.push(name_3);
+name_of_the_student_array.push(name_4);
+
+console.log(name_of_the_student_array);
+
+document.getElementsById("display_name").innerHTML = name_of_the_student_array
+document.getElementsById("submit_button").style.displat = "none";
+document.getElementsById("sort_button").style.display = "inline-block";
+
+function sorting (
+    name_of_the_student_array.sort()
+    console.log(name_of_the_student_array);
+    document.getElementsById("display_name").innerHTML = name_of_the_student_array
+)
